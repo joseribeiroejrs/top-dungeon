ab3843c89bb7b86c2524b1df62ffebde81679191
Eduardo Soares <eduardodeiturama@hotmail.com>
2021-10-16 06:50:05 -0300

    Iniciando tutorial

---
+++ Assets/README.txt
@@ -0,0 +1,2 @@
+Seguindo o tutorial https://www.youtube.com/watch?v=b8YUfee_pzc
+Parei em 59:17
---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+	public Transform lookAt;
+	public float boundX = 0.15f;
+	public float boundY = 0.05f;
+
+	private void LateUpdate()
+	{
+		Vector3 delta = Vector3.zero;
+		float deltaX = lookAt.position.x - transform.position.x;
+
+		if (deltaX > boundX || deltaX < -boundX)
+		{
+			if (transform.position.x < lookAt.position.x)
+			{
+
+			}
+		}
+	}
+}
